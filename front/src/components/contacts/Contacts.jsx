8a9da087a46e839b428d6b2e7c60f00d2e17cbad
lejodev <alejandro.rv97@gmail.com>
2021-12-23 15:48:21 -0500

    Working on contacts modal select

--- front/src/components/contacts/Contacts.jsx
+++ front/src/components/contacts/Contacts.jsx
@@ -1,14 +1,32 @@
 import React, { useState, useEffect } from "react";
+import ModalAddContact from "../modals/add/ModalAddContact";
 import Search from "./search/Search";
 import ExportImport from "./export-import-add/ExportImport";
 import ContactsTable from "./contacts-table/ContactsTable";
 import "./_contacts.scss";
 
 const Contacts = () => {
+  const [modalIsOpen, setmodalIsOpen] = useState(false);
+
+  function onClose() {
+    setmodalIsOpen(false);
+  }
+
   return (
     <div className="contacts-general-container">
       <Search />
-      <ExportImport />
+      <div className="export-import">
+        <button>IMPORT</button>
+        <button>EXPORT</button>
+        <button
+          onClick={() => {
+            setmodalIsOpen(true);
+          }}
+        >
+          ADD
+        </button>
+      </div>
+      <ModalAddContact isOpen={modalIsOpen} onClose={onClose} />
       <ContactsTable />
     </div>
   );
---
+++ front/src/components/modals/add/ModalAddContact.jsx
@@ -0,0 +1,160 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+import "../modalStyles/_modalContacts.scss";
+
+const ModalAddContact = ({ isOpen, onClose }) => {
+  const [regionInitialValue, setRegionInitialValue] = useState(); //////
+  const [regions, setRegions] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [cities, setCities] = useState([]);
+
+  var currentRegion;
+  var currentCountry;
+  var cirrentCity;
+  var getCities;
+  var getRegions;
+
+  useEffect(() => {
+    async function setLocation() {
+      getRegions = await fetch("http://localhost:3050/location/region");
+      let regions = await getRegions.json();
+      setRegions(regions);
+    }
+    setLocation();
+    // getCountries();
+  }, []);
+
+  async function getCountries(regionId) {
+    getCountries = await fetch(
+      `http://localhost:3050/location/country/${regionId}`
+    );
+    let countries = await getCountries.json();
+    console.log(countries);
+    setCountries(countries);
+  }
+  const { handleSubmit, register, reset } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  if (isOpen) {
+    return (
+      <div className="modal-contact">
+        <form onSubmit={handleSubmit(onSubmit)} className="modal-contact-form">
+          <div className="main">
+            <label htmlFor="name">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                {...register("name", { required: true })}
+              />
+            </label>
+            <label htmlFor="lastName">
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                {...register("lastName", { required: true })}
+              />
+            </label>
+            <label htmlFor="position">
+              <input
+                type="text"
+                name="position"
+                id="position"
+                {...register("position", { required: true })}
+              />
+            </label>
+            <label htmlFor="email">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                {...register("email", { required: true })}
+              />
+            </label>
+            <label htmlFor="company">
+              <input
+                type="text"
+                name="company"
+                id="company"
+                {...register("company", { required: true })}
+              />
+            </label>
+          </div>
+          <label htmlFor="location">
+            <select
+              name="region"
+              id="region"
+              {...register("region", { required: true })}
+              onChange={(e) => {
+                let regionId = e.target.value;
+                console.log(e.target.value);
+                fetch(`http://localhost:3050/location/country/${regionId}`)
+                  .then((resp) => resp.json())
+                  .then((countries) => setCountries(countries));
+              }}
+              onClick={(e) => {
+                console.log(e.target.value);
+                console.log(countries);
+              }}
+            >
+              {regions.map((region) => (
+                <option key={region._id} value={region._id}>
+                  {region.name}
+                </option>
+              ))}
+              {/* <option value="eut">eur</option>
+              <option value="afr">afr</option> */}
+            </select>
+            <select
+              name="country"
+              id="country"
+              {...register("country", { required: true })}
+            >
+              {/* {countries.map((country) => (
+                <option value={country._id}>{country.name}</option>
+              ))} */}
+            </select>
+            <select
+              name="city"
+              id="city"
+              {...register("city", { required: true })}
+            >
+              <option value="med">med</option>
+              <option value="car">car</option>
+              <option value="man">man</option>
+            </select>
+          </label>
+          <label htmlFor="address">
+            <input
+              type="text"
+              name="address"
+              id="address"
+              {...register("address", { required: true })}
+            />
+          </label>
+          <select
+            name="interest"
+            id="interest"
+            {...register("interest", { required: true })}
+          >
+            <option value="0">0</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="75">75</option>
+            <option value="100">100</option>
+          </select>
+          <input type="submit" value="SAVE CONTACT" />
+        </form>
+        <button onClick={onClose}>CLOSE</button>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default ModalAddContact;
